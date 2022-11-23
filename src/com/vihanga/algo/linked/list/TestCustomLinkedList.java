d231273162c7679c2ae09408caa72eb2f147c728
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-11-24 04:03:31 +0530

    Challenge:Remove the K th element from the custom LinkedList.

--- src/com/vihanga/algo/linked/list/TestCustomLinkedList.java
+++ src/com/vihanga/algo/linked/list/TestCustomLinkedList.java
@@ -7,19 +7,42 @@ import com.vihanga.algo.linked.list.operators.Operator;
 public class TestCustomLinkedList {
 
     public static void main(String[] args) {
+
+
         CustomLinkedList customLinkedList = new CustomLinkedList();
 
         Node firstNode = new Node(15);
         Node secondNode = new Node(12);
         Node thirdNode =  new Node(1);
         Node fourthNode = new Node(8);
+        Node fifthNode = new Node(66);
+        Node sixthNode = new Node(84);
+        Node seventhNode = new Node(78);
+        Node eighthNode = new Node(82);
+        Node ninethNode = new Node(81);
+        Node tenthNode = new Node(680);
+        Node eleventhNode = new Node(87);
+        Node twovelvethNode = new Node(28);
+        Node thirteenNode = new Node(85);
+
 
         customLinkedList.head = firstNode;
         firstNode.next = secondNode;
         secondNode.next = thirdNode;
         thirdNode.next = fourthNode;
-        // fourth.next = null already.
+        fourthNode.next =fifthNode;
+        fifthNode.next =sixthNode;
+        sixthNode.next = seventhNode;
+        seventhNode.next =eighthNode;
+        eighthNode.next = ninethNode;
+        ninethNode.next =tenthNode;
+        tenthNode.next=eleventhNode;
+        eleventhNode.next = twovelvethNode;
+        twovelvethNode.next = thirteenNode;
+        // Last node (last.next = null) is  already null.
 
+        /*
+        // Training
         customLinkedList.displayContent();
         Operator operator = new Operator();
         operator.deleteBackHalfLinkedList(customLinkedList);
@@ -28,5 +51,24 @@ public class TestCustomLinkedList {
       CustomLinkedList customLinkedList1=  new CustomLinkedList();
         operator.deleteBackHalfLinkedList(customLinkedList1);
         System.out.println("   ...    ");
+         */
+
+//        Coding challenge
+        customLinkedList.displayContent();
+        Operator operator = new Operator();
+     operator.deleteKElementFromBack(customLinkedList,5);
+        customLinkedList.displayContent();
+        operator.deleteKElementFromBack2(customLinkedList,2);
+        customLinkedList.displayContent();
+//        System.out.println(" ....... Base Cases ");
+//        CustomLinkedList customLinkedList1 = new CustomLinkedList();
+//        operator.deleteKElementFromBack(customLinkedList1,8);
+//        customLinkedList1.displayContent();
+//        System.out.println(" ....... Base cases");
+//        operator.deleteKElementFromBack( customLinkedList,0);
+//        customLinkedList.displayContent();
+//        System.out.println(" .... ");
+
+
     }
 }
--- src/com/vihanga/algo/linked/list/operators/Operator.java
+++ src/com/vihanga/algo/linked/list/operators/Operator.java
@@ -26,4 +26,85 @@ public class Operator {
         prevNode.next = null;
      return    customLinkedList;
     }
+
+// Deleting the K th node from bak of the custom Linked list
+    public CustomLinkedList deleteKElementFromBack(CustomLinkedList customLinkedList,int k){
+
+        if(customLinkedList == null || customLinkedList.head ==null ){
+            customLinkedList.head = null;
+            return customLinkedList;
+        }
+        if (k==0){
+            return customLinkedList;
+        }
+
+        Node fastTrack = customLinkedList.head;
+        Node backTrack = customLinkedList.head;
+        Node tempNode = null;
+
+        int count =0;
+        while (fastTrack !=null && fastTrack.next!=null){
+            fastTrack = fastTrack.next;
+            count ++;
+        }
+//        int numIteration = count-k;
+//        while (numIteration>0){
+//            backTrack = backTrack.next;
+//            prevNode = backTrack;
+//            numIteration--;
+//        }
+//
+        int secondCount =0;
+        int eliminationPoint = count-k;
+        System.out.println("eliminationPoint: "+eliminationPoint);
+
+        while (backTrack.next != null){
+            if(secondCount ==eliminationPoint ){
+                System.out.println(backTrack.data);
+                backTrack.next =backTrack.next.next;
+
+                backTrack =tempNode;
+               break;
+
+            }else {
+                backTrack = backTrack.next;
+            }
+            secondCount++;
+        }
+
+
+//        prevNode.next = null;
+        return customLinkedList;
+    }
+
+    public void deleteKElementFromBack2(CustomLinkedList customLinkedList, int k){
+        if(customLinkedList.head == null || k==0){
+            return ;
+        }
+
+        Node first = customLinkedList.head;
+        Node second = customLinkedList.head;
+
+        for (int i=0;i<k;i++){
+            second  = second.next;
+            if(second.next == null){
+                if(i==k-1){
+                    customLinkedList.head = customLinkedList.head.next;
+
+                }
+//                when 0<k>customLinkedList.length, does not modified the list
+                return ;
+            }
+        }
+
+        while (second.next != null){
+            first = first.next;
+            second = second.next;
+        }
+
+        first.next =first.next.next;
+    }
+
 }
+
+
