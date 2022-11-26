9a100c7b7131de9c42271caeb774c5405c0d2235
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-11-27 01:50:21 +0530

    Playing with string builder class.

--- src/com/vihanga/interview/prep/string/Operator.java
+++ src/com/vihanga/interview/prep/string/Operator.java
@@ -3,8 +3,9 @@ package com.vihanga.interview.prep.string;
 public class Operator {
 
     public static void main(String[] args) {
-        String output = firstLetterCapital("Hi, this is Vihanga malinda.");
-        System.out.println(output);
+//        String output = firstLetterCapital("Hi, this is Vihanga malinda.");
+//        System.out.println(output);
+        stringMutation("ynmnimj mbgjkiuim");
     }
 
     public static String firstLetterCapital(String input) {
@@ -19,5 +20,36 @@ public class Operator {
             word.delete(0, word.length());
         }
         return sentence.toString().trim();
+    }
+
+    public static void stringMutation(String s){
+        StringBuilder builder = new StringBuilder(s);
+        builder.append("viha bgw");
+        builder.append("nmnkjjnbg");
+        System.out.println();
+        System.out.println("length: "+builder.length()+ " , value: "+builder.toString());
+
+
+        builder.insert(5,"NEw Letter");
+        System.out.println();
+        System.out.println("length: "+builder.length()+ " , value: "+builder.toString());
+
+        System.out.println();
+        System.out.println("char at 7:  "+builder.charAt(7));
+        System.out.println("char at 33:  "+builder.charAt(33));
+
+
+        builder.delete(7,33);
+        System.out.println();
+        System.out.println("length: "+builder.length()+ " , value: "+builder.toString());
+
+
+        builder.deleteCharAt(0);
+        System.out.println();
+        System.out.println("length: "+builder.length()+ " , value: "+builder.toString());
+
+
+
+
     }
 }
